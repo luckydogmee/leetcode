ac5fe9b9b760cb060adc0d7304069d21359713a7
Ray Lay <raylay@RaydeiMac.lan>
2019-12-28 13:30:02 +0800

    开发环境

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,12 +1,12 @@
-// export default (str) => {
-//   // 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-//   let arr = str.split(' ')
-//   // 对数组进行遍历，然后每个元素进行反转
-//   let result = arr.map(item => {
-//     return item.split('').reverse().join('')
-//   })
-//   return result.join(' ')
-// }
+
+/* 字符串按数组分割，要求分别翻转每个字符串 */
+const reverse = (str) => {
+  let arr = str.split(' ')
+  let result = arr.map(item => {
+    return item.split('').reverse().join('')
+  })
+  return result.join(' ')
+}
 
 // export default (str) => {
 //   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
@@ -31,3 +31,5 @@ export default (str) => {
     return item.split('').reverse().join('')
   }).join(' ')
 }
+
+export { reverse }
